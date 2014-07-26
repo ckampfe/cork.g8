9705990517f8c5eab8648382b508d4bbecdd5faa
Clark Kampfe <clark@bellycard.com>
2014-07-26 17:43:56 -0500

    build file indentation

--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -23,20 +23,21 @@ object CorkBuild extends Build {
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       libraryDependencies ++= Seq(
-        "org.scalatra"           %% "scalatra"             % ScalatraVersion,
-        "org.scalatra"           %% "scalatra-scalate"     % ScalatraVersion,
-        "org.scalatra"           %% "scalatra-specs2"      % ScalatraVersion % "test",
-        "ch.qos.logback"          % "logback-classic"      % "1.0.6" % "runtime",
-        "org.scalatra"           %% "scalatra-json"        % "2.3.0",
-        "org.json4s"             %% "json4s-jackson"       % "3.2.10",
-        "com.github.nscala-time" %% "nscala-time"          % "1.2.0",
-        "org.scalikejdbc"        %% "scalikejdbc"          % "2.0.+",
-        "mysql"                   % "mysql-connector-java" % "5.1.31",
-        // "com.h2database"          %  "h2"                  % "1.4.+",
-        "ch.qos.logback"          %  "logback-classic"     % "1.1.+",
-
-        "org.eclipse.jetty"       % "jetty-webapp"     % "8.1.8.v20121106" % "container",
-        "org.eclipse.jetty.orbit" % "javax.servlet"    % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
+        "org.scalatra"            %% "scalatra"             % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-scalate"     % ScalatraVersion,
+        "org.scalatra"            %% "scalatra-specs2"      % ScalatraVersion % "test",
+        "ch.qos.logback"           % "logback-classic"      % "1.0.6" % "runtime",
+        "org.scalatra"            %% "scalatra-json"        % "2.3.0",
+        "org.json4s"              %% "json4s-jackson"       % "3.2.10",
+        "com.github.nscala-time"  %% "nscala-time"          % "1.2.0",
+        "org.scalikejdbc"         %% "scalikejdbc"          % "2.0.+",
+        "mysql"                    % "mysql-connector-java" % "5.1.31",
+        "com.h2database"           %  "h2"                  % "1.4.+",
+        "ch.qos.logback"           %  "logback-classic"     % "1.1.+",
+        "com.typesafe.akka"       %% "akka-actor"           % "2.3.4",
+        "net.databinder.dispatch" %% "dispatch-core"        % "0.11.1",
+        "org.eclipse.jetty"       % "jetty-webapp"          % "8.1.8.v20121106" % "container",
+        "org.eclipse.jetty.orbit" % "javax.servlet"         % "3.0.0.v201112011016" % "container;provided;test" artifacts (Artifact("javax.servlet", "jar", "jar"))
       ),
       scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
         Seq(
