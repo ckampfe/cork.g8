75e35ba876afd53a4b4a1ebea7e8996d126cf9cf
Clark Kampfe <clark@bellycard.com>
2014-08-09 14:54:59 -0500

    working flyway config

--- src/main/g8/project/build.sbt
+++
@@ -1,5 +0,0 @@
-seq(flywaySettings: _*)
-
-flywayUrl := "jdbc:mysql://localhost:3306/$name;format="snake"$"
-
-flywayUser := "root"
--- src/main/g8/project/build.scala
+++ src/main/g8/project/build.scala
@@ -3,25 +3,31 @@ import Keys._
 import org.scalatra.sbt._
 import org.scalatra.sbt.PluginKeys._
 import com.mojolly.scalate.ScalatePlugin._
+import org.flywaydb.sbt.FlywayPlugin._
 import ScalateKeys._
 
 object CorkBuild extends Build {
-  val Organization = "$organization$"
-  val Name = "$name$"
-  val Version = "$version$"
-  val ScalaVersion = "$scala_version$"
+  val Organization    = "$organization$"
+  val Name            = "$name$"
+  val Version         = "$version$"
+  val ScalaVersion    = "$scala_version$"
   val ScalatraVersion = "$scalatra_version$"
 
   lazy val project = Project (
     "cork",
     file("."),
-    settings = Defaults.defaultSettings ++ ScalatraPlugin.scalatraWithJRebel ++
-    scalateSettings ++ Seq(giter8.ScaffoldPlugin.scaffoldSettings: _*) ++ Seq(
+    settings = Defaults.defaultSettings ++
+    ScalatraPlugin.scalatraWithJRebel ++
+    Seq(giter8.ScaffoldPlugin.scaffoldSettings: _*) ++
+    seq(org.flywaydb.sbt.FlywayPlugin.flywaySettings: _*) ++
+    Seq(
       organization := Organization,
-      name := Name,
-      version := Version,
+      name         := Name,
+      version      := Version,
       scalaVersion := ScalaVersion,
-      resolvers += Classpaths.typesafeReleases,
+      resolvers    += Classpaths.typesafeReleases,
+      flywayUrl    := "jdbc:mysql://localhost:3306/scalatra_test",
+      flywayUser   := "root",
       libraryDependencies ++= Seq(
         "org.scalatra"            %% "scalatra"             % ScalatraVersion,
         "org.scalatra"            %% "scalatra-scalate"     % ScalatraVersion,
