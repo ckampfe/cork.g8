961fe6196a55bf00f56a906ea991c88862390304
Clark Kampfe <clark@bellycard.com>
2014-08-09 15:42:47 -0500

    create an api table instead
    for now: let user create the matching db manually

--- src/main/g8/src/main/resources/db/migration/V0__Create_$name__snake$_development_database.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE $name;format="snake"$_development IF NOT EXISTS
-CHARACTER SET = "utf8"
---
+++ src/main/scaffolds/api/src/main/resources/db/migration/V0__Create_$name__snake$s_table.sql
@@ -0,0 +1,3 @@
+CREATE TABLE $name;format="snake"$_development IF NOT EXISTS
+  ID int NOT NULL PRIMARY KEY
+  CHARACTER SET = "utf8"
