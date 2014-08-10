9f1c1d99910305f1702338ae27f47144a1dad770
Clark Kampfe <clark@bellycard.com>
2014-08-10 15:36:11 -0500

    build out api initial migration
    string types now required for api by default, no Option[String]

--- src/main/scaffolds/api/src/main/resources/db/migration/V0__Create$api_name__Camel$s.sql
+++ src/main/scaffolds/api/src/main/resources/db/migration/V0__Create$api_name__Camel$s.sql
@@ -1,3 +1,7 @@
-create table $api_name;format="lower,snake"$ (
-  ID int NOT NULL PRIMARY KEY
+create table $api_name;format="lower,snake"$s (
+  id int not null primary key auto_increment,
+  kind varchar(100) not null,
+  size varchar(100) not null,
+  created_at timestamp not null,
+  updated_at timestamp not null
 );
