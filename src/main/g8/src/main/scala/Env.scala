b83434fb6128d97f6cc3a07718687f32ed3ac307
Clark Kampfe <clark@bellycard.com>
2014-08-17 20:06:00 -0500

    spec helper, test and dev loaders

--- src/main/g8/src/main/scala/Env.scala
+++ src/main/g8/src/main/scala/Env.scala
@@ -3,19 +3,18 @@ package environment
 import com.typesafe.config._
 
 object Env {
-  val Config = ConfigFactory.load()
+  var Config = ConfigFactory.load()
 
-  val env = Config.getString("$name;format="norm"$.env")
+  lazy val env = Config.getString("$name;format="norm"$.env")
 
   object Db {
-    val adapter  = Config.getString("$name;format="norm"$.database.adapter")
-    val host     = Config.getString("$name;format="norm"$.database.host")
-    val port     = Config.getString("$name;format="norm"$.database.port")
-    val name     = Config.getString("$name;format="norm"$.database.name")
-    val user     = Config.getString("$name;format="norm"$.database.user")
-    val password = Config.getString("$name;format="norm"$.database.password")
+    lazy val adapter  = Config.getString("$name;format="norm"$.database.adapter")
+    lazy val host     = Config.getString("$name;format="norm"$.database.host")
+    lazy val port     = Config.getString("$name;format="norm"$.database.port")
+    lazy val name     = Config.getString("$name;format="norm"$.database.name")
+    lazy val user     = Config.getString("$name;format="norm"$.database.user")
+    lazy val password = Config.getString("$name;format="norm"$.database.password")
 
-    val url = s"jdbc:\${adapter}://\${host}:\${port}/\${name}_\${env}"
+    lazy val url = s"jdbc:\${adapter}://\${host}:\${port}/\${name}"
   }
 }
-
--- src/main/g8/src/test/scala/SpecHelper.scala
+++ src/main/g8/src/test/scala/SpecHelper.scala
@@ -1,9 +1,15 @@
 package $package$
 
 import org.scalatest._
+import com.typesafe.config._
+import environment._
 
 abstract class SpecBase extends FunSpec
   with Matchers
   with OptionValues
   with Inside
   with Inspectors
+  with DatabaseConnector
+{
+  environment.Env.Config = ConfigFactory.load("application-test")
+}
